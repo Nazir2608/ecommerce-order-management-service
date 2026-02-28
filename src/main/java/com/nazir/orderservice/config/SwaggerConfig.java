df56782dd122d0c842f30703502106bff23353c7
Md Nazir <mdnazir2608@gmail.com>
2026-02-28 13:16:55 +0530

    added pre seeded admin for developement

--- src/main/java/com/nazir/orderservice/config/SwaggerConfig.java
+++ src/main/java/com/nazir/orderservice/config/SwaggerConfig.java
@@ -7,16 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(
-        title = "Orderservice API",
-        version = "1.0.0",
-        description = "Production-ready Order Management System"
-))
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
+@OpenAPIDefinition(info = @Info(title = "Orderservice API", version = "1.0.0", description = "Production-ready Order Management System"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class SwaggerConfig {
 }
